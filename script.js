d263ca3db404cdace00d0428a72b5c2608cd66c8
Maduzan <maduzan.manmatharajah@gmail.com>
2025-07-19 14:27:10 +0200

    Clean version du portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+  const navMobile = document.querySelector(".nav-mobile-menu");
+  const mobileOverlay = document.querySelector(".mobile-overlay");
+
+  if (navMobile.classList.contains("active")) {
+    navMobile.classList.remove("active");
+    mobileOverlay.classList.remove("active");
+  } else {
+    navMobile.classList.add("active");
+    mobileOverlay.classList.add("active");
+  }
+}
